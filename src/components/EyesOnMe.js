d88542c363fa73dfbb41b68be4bf5abac756b033
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-11-05 20:02:03 +0300

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  // Event handler for focus event
+  function handleFocus() {
+    console.log('Good!');
+  }
+
+  // Event handler for blur event
+  function handleBlur() {
+    console.log('Hey! Eyes on me!');
+  }
+
+  return (
+    <button 
+      onFocus={handleFocus} 
+      onBlur={handleBlur}
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  // Event handler for input change
+  function handleChange() {
+    console.log('Entering password...');
+  }
+
+  return (
+    <input 
+      type="password" 
+      onChange={handleChange} 
+    />
+  );
 }
 
 export default Keypad;
